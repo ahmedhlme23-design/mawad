ebd975a0e9b80c98378b313851abf06a1f26db16
ahmedhlme23 <ahmedhlme23@gmail.com>
2026-08-12 01:46:36 +0300

    Refactor database connection settings and remove unused imports in db.ts

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,12 +1,7 @@
 import { PrismaClient } from '@prisma/client';
-import { PrismaPg } from '@prisma/adapter-pg';
-import { Pool } from 'pg';
 
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
-const pool = new Pool({ connectionString: process.env.POSTGRES_PRISMA_URL });
-const adapter = new PrismaPg(pool);
-
-export const prisma = globalForPrisma.prisma || new PrismaClient({ adapter });
+export const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
